7d9d276e47baafbf7e9edf1ac83b934a07331640
aizabaw <fireandaiz@gmail.com>
2018-06-11 16:18:02 +0800

    removed unnecessary files from Tomcat

--- banana-api/src/main/java/banana/main/api/controller/UserController.java
+++ banana-api/src/main/java/banana/main/api/controller/UserController.java
@@ -3,6 +3,7 @@ package banana.main.api.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import banana.db.mongo.dao.UserDao;
@@ -10,9 +11,7 @@ import banana.main.objects.User;
 
 @RestController
 public class UserController {
-	
-//	@RequestMapping(method=RequestMethod.GET, path="/users")
-	@RequestMapping("/users")
+	@RequestMapping(method=RequestMethod.GET, path="/users")
 	public List<User> getAllUsers() {
 		
 		UserDao userdao = new UserDao();
